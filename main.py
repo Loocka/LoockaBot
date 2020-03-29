94f59b0cdf5c2072e127efb55d9248f57fdc9aac
Loocka <vk.misharin@yandex.ru>
2020-03-29 17:57:27 +0500

    Implement rapid game restart iteration

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from sc2 import run_game, maps, Race, Difficulty
+from importlib import reload
+
+import sc2
+from sc2 import Race, Difficulty
 from sc2.player import Bot, Computer
 
-from bot import LoockaBot
+import bot
 
 
-run_game(maps.get("CatalystLE"), [
-    Bot(Race.Terran, LoockaBot()),
-    Computer(Race.Terran, Difficulty.VeryHard)
-], realtime=False)
+def main():
+    player_config = [
+        Bot(Race.Random, bot.LoockaBot()),
+        Computer(Race.Random, Difficulty.VeryHard)
+    ]
+
+    gen = sc2.main._host_game_iter(
+        sc2.maps.get("CatalystLE_NO_AI"),
+        player_config,
+        realtime=False
+    )
+
+    while True:
+        next(gen)
+
+        if input('Press enter to reload or type "q" to exit: ') == 'q':
+            exit()
+
+        reload(bot)
+        player_config[0].ai = bot.LoockaBot()
+        gen.send(player_config)
+
+
+if __name__ == "__main__":
+    main()
